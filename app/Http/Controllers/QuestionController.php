3de91f6bec91f54d4618ab514928b8a587470c15
Jose Guilherme Sorio <joseguilhermesorio@gmail.com>
2023-10-19 21:04:25 -0300

    test: finish tests of creating a question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+use Closure;
 
 class QuestionController extends Controller
 {
     public function store(): RedirectResponse
     {
         Question::create(request()->validate([
-            'question' => ['required']
+            'question' => [
+                'required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if (trim($value[-1]) !== '?') {
+                        $fail("Are you sure that is a question ?");
+                    }
+                }
+            ]
         ]));
         return redirect()->route('dashboard');
     }
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -6,7 +6,7 @@ use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\post;
 
-it('should be able create a question bigger than 255 characters', function () {
+it('should be able create a new question bigger than 255 characters', function () {
     // Arrange -> Preparar
     $user = User::factory()->create();
     actingAs($user);
@@ -24,9 +24,32 @@ it('should be able create a question bigger than 255 characters', function () {
 });
 
 it('should check if ends with a question mark ?', function () {
+    // Arrange
+    $user = User::factory()->create();
+    actingAs($user);
 
+    // Act
+    $data = [
+        'question' => str_repeat('*', 30)
+    ];
+    $request = post(route('question.store'), $data);
+
+    // Assert
+    $request->assertSessionHasErrors(['question' => 'Are you sure that is a question ?']);
+    assertDatabaseCount('questions', 0);
 });
 
 it('should have at least 10 characters', function () {
+    // Arrange
+    $user = User::factory()->create();
+    actingAs($user);
 
+    // Act
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8)
+    ]);
+
+    // Assert
+    $request->assertSessionHasErrors('question');
+    assertDatabaseCount('questions', 0);
 });
