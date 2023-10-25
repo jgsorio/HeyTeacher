3d13915c20a93d03020936b8403f08d95fe58476
Jose Guilherme Sorio <joseguilhermesorio@gmail.com>
2023-10-24 22:07:13 -0300

    refactor: make components view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,15 +12,14 @@
                     <span class="font-medium">Opss...</span> {{ $error }}
                 </div>
             @endforeach
-            <form action="{{ route('question.store') }}" method="post">
-                @csrf
+            <x-form action="{{ route('question.store') }}">
                 <div>
                     <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question</label>
                     <textarea id="message" name="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."> {{ old('question') ?? '' }}</textarea>
                 </div>
-                <button type="submit" class="py-2.5 px-5 mt-4 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Send Message</button>
-                <button type="reset" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancel</button>
-            </form>
+                <x-button type="submit" label="Send Message"></x-button>
+                <x-button type="reset" label="Cancel" class="dark:bg-red-900 hover:dark:bg-red-700"></x-button>
+            </x-form>
         </div>
     </div>
 </x-app-layout>
